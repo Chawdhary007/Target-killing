75c9accbca66cd242172f0e3a46c0fd866876697
chawdhary <44389314+chawdhary@users.noreply.github.com>
2018-11-06 20:29:45 +0500

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,19 @@
+pkg install gem
+gem install lolcat
+
+echo      " Missing packajes installing plz wait " |  lolcat  -a -d 50 -s 13
+apt update
+apt upgrade
+pkg install python2
+pip2 install --upgrade pip
+pip2 install mechanize
+pkg install git
+git clone https://github.com/Chawdhary007/Target-killing
+cd Target-killing
+chmod +x T-killing.py
+echo  "Now use Target Killing  by python2 T-killing" lolcat
+ sleep 14
+
+
+
+
