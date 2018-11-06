417f02f27bb958ecf56b74718a8e70a878cb83ec
chawdhary <44389314+chawdhary@users.noreply.github.com>
2018-11-06 21:00:30 +0500

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,4 @@
-pkg install gem
+pip2 install gem
 gem install lolcat
 
 echo      " Missing packajes installing plz wait " |  lolcat  -a -d 50 -s 13
@@ -8,11 +8,10 @@ pkg install python2
 pip2 install --upgrade pip
 pip2 install mechanize
 pkg install git
-git clone https://github.com/Chawdhary007/Target-killing
-cd Target-killing
 chmod +x T-killing.py
-echo  "Now use Target Killing  by python2 T-killing" lolcat
- sleep 14
+echo  "Now use Target Killing  by python2 T-killing" | lolcat
+echo " if u have no passlist then just type >> Hack.txt <<" | lolcat
+sleep 14
 
 
 
